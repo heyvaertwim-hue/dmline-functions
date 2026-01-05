ab88a921701a9b2a3268c7878daf1f204d454984
heyvaertwim-hue <heyvaert.wim@gmail.com>
2026-01-05 17:46:12 +0100

    Create health.js

---
+++ src/functions/health.js
@@ -0,0 +1,12 @@
+const { app } = require("@azure/functions");
+
+app.http("health", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async () => {
+    return {
+      status: 200,
+      body: "OK"
+    };
+  }
+});
