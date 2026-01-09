b5263b27ef259ffb286e70e0634880102967fd28
heyvaertwim-hue <heyvaert.wim@gmail.com>
2026-01-09 15:33:18 +0100

    Update index.js

--- src/functions/ping/index.js
+++ src/functions/ping/index.js
@@ -1,10 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
-    body: {
-      status: "pong",
-      function: "ping",
-      source: "GitHub"
-    }
+    body: "pong"
   };
 };
